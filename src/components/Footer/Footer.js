98932a1e69191b514a2f180647f170e164ffa897
vudd95 <vu.dd95@gmail.com>
2023-10-30 19:21:38 +0700

    update homepage login register

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,24 +1,72 @@
-import React from 'react'
-import '../Footer/Footer.css'
+import React from "react";
+import "../Footer/Footer.css";
 
 const Footer = () => {
   return (
-    <div className='footer'>
-        <div className='footer__top'>
-            <div className='footer-logo'>
-                <div className='footer-logo-image'></div>
-                <p>Best information about the company gies here but now lorem ipsum is</p>
-            </div>
-            <div className='footer-about'></div>
-            <div className='footer-partnership'></div>
-            <div className='footer-information'></div>
-            <div className='footer-forUsers'></div>
-            <div className='footer-getApp'></div>
+    <div className="footer">
+      <div className="footer__top">
+        <div className="footer-logo">
+          <img src="/images/logo-colored.png"></img>
+          <p>
+            Best information about the company gies here but now lorem ipsum is
+          </p>
+          <div className="footer-logo__icon">
+            <img src="/images/facebook.png"></img>
+            <img src="/images/facebook.png"></img>
+            <img src="/images/facebook.png"></img>
+            <img src="/images/facebook.png"></img>
+            <img src="/images/facebook.png"></img>
+          </div>
         </div>
-        <div className='footer__bottom' ></div>
-        
+        <div className="footer-about">
+          <h5>About</h5>
+          <ul>
+            <li>About us</li>
+            <li>Find store</li>
+            <li>Categories</li>
+            <li>Blogs</li>
+          </ul>
+        </div>
+        <div className="footer-partnership">
+          <h5>Partnership</h5>
+          <ul>
+            <li>About us</li>
+            <li>Find store</li>
+            <li>Categories</li>
+            <li>Blogs</li>
+          </ul>
+        </div>
+        <div className="footer-information">
+          <h5>Information</h5>
+          <ul>
+            <li>Help Center</li>
+            <li>Money Refund</li>
+            <li>Shipping</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+        <div className="footer-forUsers">
+          <h5>For Users</h5>
+          <ul>
+            <li>Login</li>
+            <li>Register</li>
+            <li>Settings</li>
+            <li>My Order</li>
+          </ul>
+        </div>
+        <div className="footer-getApp">
+        <h5>Get App</h5>
+              <ul>
+                <li><img src="/images/google-play.png"></img></li>
+                <li><img src="/images/app-store.png"></img></li>
+              </ul>
+        </div>
+      </div>
+      <div className="footer__bottom">
+        <p>© 2023 Ecommerce.</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login">
+      <div className="login-content">
+        <div className="div-content__logo">
+          <img src="/images/logo-colored.png"></img>
+        </div>
+        <div className="div-content__welcomeback">
+          <h1>Welcome Back</h1>
+        </div>
+        <div className="div-content__switchPage">
+          <ul>
+            <li className="active">
+              <Link to={"/login"}>Sign in</Link>
+            </li>
+            <li>
+              <Link to={"/signup"}>Sign up</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="div-content__input">
+          <input placeholder="Email"></input>
+          <input placeholder="Password"></input>
+        </div>
+        <div className="div-content__button">
+          <button>
+            Sign in
+          </button>
+          <button>
+            <Link to={"/signup"}>Sign up</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
